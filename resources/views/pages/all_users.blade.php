19444ba6ad89cd0e0b545d0178e8fbfa4320288e
Beatriz Cruz <beatrizcruz@Beatrizs-MacBook-Air.local>
2023-11-22 15:40:55 +0000

    botões

--- resources/views/pages/all_users.blade.php
+++ resources/views/pages/all_users.blade.php
@@ -8,7 +8,7 @@
     <form method="GET" action="{{ route('show.users', ['pageNr' => 1]) }}">
         <input type="text" name="query" placeholder="Search users..." value="{{ $query }}">
         <button type="submit">Search</button>
-    </form>
+    </form> 
 
     @if ($users->isEmpty())
         <p>No users found.</p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,13 +3,9 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>User Information</h1>
+    <h1>Welcome to StyleSwap!</h1>
     @if(Auth::check())
-        <p><strong>Name:</strong> {{ Auth::user()->username }}</p>
-        <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
-        <p><strong>First Name:</strong> {{ Auth::user()->first_name }}</p>
-        <p><strong>Last Name:</strong> {{ Auth::user()->last_name }}</p>
-        <p><strong>Balance:</strong> {{ Auth::user()->balance }}€</p>
+        <p>LOGED</p>
     @else
         <p>User is not logged in.</p>
     @endif
